c878557e7b7b2dfa718fda0b88d4be4f28b46a97
Ashe Visavale <flamingqaz@gmail.com>
2023-03-23 20:43:35 -0400

    made a little skele guy with a little walk
    also deleted most of the art stuff jackson sent, there were like 4000 files, no way i was going to go through that, we will just find stuff as we need it

---
+++ Assets/Scripts/playerAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using SwordData;
+
+public class playerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Animator playerAnim;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerAnim = GetComponent<Animator>();
+    }
+
+    public void movementAnim()
+    {
+        playerAnim.SetBool("Moving", Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0 || Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0);
+
+        Vector2 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        Vector2 mouse = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+        
+        if (mouse.x < playerScreenPoint.x) 
+            GetComponent<SpriteRenderer>().flipX = true;
+        else 
+            GetComponent<SpriteRenderer>().flipX = false;
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private playerAnimation playerAnimation;
+    
+    [SerializeField]
+    private float moveSpeed;
+
+
+    void Awake()
+    {
+        playerAnimation = GetComponent<playerAnimation>();
+    }
+
+    void FixedUpdate()
+    {
+        playerAnimation.movementAnim();
+        transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed);
+    }
+}

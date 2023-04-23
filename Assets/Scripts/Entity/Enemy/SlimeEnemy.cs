a024e0607b799c69b7b9792a40dd1b5afe5e98f4
Cannicide <41762964+Cannicide@users.noreply.github.com>
2023-04-23 04:41:11 -0400

    Added Slime merge behavior.

--- Assets/Scripts/Entity/Enemy/SlimeEnemy.cs
+++ Assets/Scripts/Entity/Enemy/SlimeEnemy.cs
@@ -7,12 +7,14 @@ public class SlimeEnemy : MonoBehaviour
 {
 
     [HideInInspector] public Enemy enemy;
-    public float maxScale = 3;
-    public float startScale = 2;
+    public float maxScale = 3f;
+    public float startScale = 2f;
+    public float mergeDelay = 10f; // Seconds before merging can occur again
+    public float mergeRange = 2f; // Range to check for other slimes to merge with
     float scale;
-    bool mergeable = true;
-    bool scaled = false;
-    bool splitting = false;
+    bool mergeable = false; // Whether this slime can currently merge
+    bool scaled = false; // Whether this slime's proper scale has been set
+    bool splitted = false; // Whether this slime has died and split into new slimes
     BoxCollider2D bc;
 
     // Start is called before the first frame update
@@ -24,12 +26,31 @@ public class SlimeEnemy : MonoBehaviour
             scale = startScale;
             SetScale();
         }
+
+        Invoke(nameof(EndMergeCooldown), mergeDelay);
+    }
+
+    void FixedUpdate() {
+        if (!mergeable || scale + 1 > maxScale) return;
+
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, mergeRange, Vector2.zero, 0f);
+        foreach (RaycastHit2D hit in hits) {
+            if (!hit) continue;
+
+            SlimeEnemy otherSlime = hit.transform.gameObject.GetComponent<SlimeEnemy>();
+            if (!otherSlime || otherSlime.gameObject == gameObject || !otherSlime.mergeable || scale != otherSlime.scale) continue;
+            if (GetInstanceID() < otherSlime.GetInstanceID()) continue; // Prevents two slimes from both trying to merging with each other at the same time
+
+            Merge(otherSlime);
+            return;
+        }
+
+        mergeable = false;
+        Invoke(nameof(EndMergeCooldown), mergeDelay);
     }
 
     public void SetScale() {
-        float value;
-        if (scale <= startScale) value = 1 / (startScale - scale + 1);
-        else value = scale / startScale;
+        float value = GetScaleModifier(scale);
 
         transform.localScale = new Vector2(value, value);
         scaled = true;
@@ -38,31 +59,33 @@ public class SlimeEnemy : MonoBehaviour
     public void Init(float newScale) {
         scale = newScale;
         SetScale();
-        mergeable = false;
         
-        float value;
-        if (scale <= startScale) value = 1 / (startScale - scale + 1);
-        else value = scale / startScale;
+        float value = GetScaleModifier(scale);
         enemy.entity.SetMaxHealth(enemy.entity.GetMaxHealth() * value);
         enemy.entity.HealPercent(100f);
     }
 
     public void Split() {
         if (scale - 1 < 0f) return;
-        if (enemy.entity.isAlive || splitting) return;
+        if (enemy.entity.isAlive || splitted) return;
 
-        splitting = true;
+        splitted = true;
         float amount = Mathf.Pow(2, startScale - (scale - 1));
         for (int i = (int) amount; i > 0; i--) {
             Instantiate(gameObject, GetRandomPosition(), transform.rotation).GetComponent<SlimeEnemy>().Init(scale - 1);
         }
     }
 
-    public void Merge(GameObject other) {
-        if (!mergeable || scale + 1 > maxScale) return;
+    public void Merge(SlimeEnemy other) {
+        Destroy(other.gameObject);
+        SlimeEnemy newSlime = Instantiate(gameObject, transform.position, transform.rotation).GetComponent<SlimeEnemy>();
 
-        Destroy(other);
-        Instantiate(gameObject, transform.position, transform.rotation).GetComponent<SlimeEnemy>().Init(scale + 1);
+        float value = GetScaleModifier(scale);
+
+        newSlime.Init(scale + 1);
+        newSlime.enemy.entity.SetMaxHealth(enemy.entity.GetMaxHealth() / value);
+        newSlime.enemy.entity.HealPercent(100f);
+        Destroy(gameObject);
     }
 
     Vector2 GetRandomPosition() {
@@ -72,6 +95,18 @@ public class SlimeEnemy : MonoBehaviour
 
         Vector2 newPos = new Vector2(Random.Range(-rectSize.x / 2, rectSize.x / 2), Random.Range(-rectSize.y / 2, rectSize.y / 2));
  
-         return (Vector2)transform.position + newPos;
+        return (Vector2)transform.position + newPos;
+    }
+
+    protected void EndMergeCooldown() {
+        mergeable = true;
+    }
+    
+    float GetScaleModifier(float scaleValue) {
+        float value;
+        if (scaleValue <= startScale) value = 1 / (startScale - scaleValue + 1);
+        else value = scaleValue / startScale;
+
+        return value;
     }
 }
